66133bb98f20b38f2ca2dab683e16e0a468ceaca
bestworld65 <marekl0605@gmail.com>
2025-06-02 21:52:17 -0400

    add ollama

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL:")
@@ -12,6 +13,17 @@ if st.button("Scrape Site"):
     cleaned_content = clean_body_content(body_content)
 
     st.session_state.dom_content = cleaned_content
-    
+     
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", cleaned_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st. text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
